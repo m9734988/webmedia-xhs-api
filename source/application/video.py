db006963418acb66263e19f3e2204d6b14fcad13
m9734988 <m9734988@users.noreply.github.com>
2026-06-08 22:42:38 +0800

    Support current RedNote video stream fields

--- source/application/video.py
+++ source/application/video.py
@@ -10,11 +10,25 @@ class Video:
         "consumer",
         "originVideoKey",
     )
+    STREAM_LINKS = (
+        "media.stream.h264[0].masterUrl",
+        "media.stream.h265[0].masterUrl",
+        "media.stream.h266[0].masterUrl",
+        "media.stream.av1[0].masterUrl",
+    )
 
     @classmethod
     def get_video_link(cls, data: Namespace) -> list:
-        return (
-            [Html.format_url(f"https://sns-video-bd.xhscdn.com/{t}")]
-            if (t := data.safe_extract(".".join(cls.VIDEO_LINK)))
-            else []
-        )
+        if links := cls.__get_stream_links(data):
+            return links
+        if t := data.safe_extract(".".join(cls.VIDEO_LINK)):
+            return [Html.format_url(f"https://sns-video-bd.xhscdn.com/{t}")]
+        return []
+
+    @classmethod
+    def __get_stream_links(cls, data: Namespace) -> list:
+        urls = []
+        for path in cls.STREAM_LINKS:
+            if url := data.safe_extract(path):
+                urls.append(Html.format_url(url))
+        return list(dict.fromkeys(i for i in urls if i))
